7fdad72f0843d27124461819468f4618db6089c7
parkjunghoon <go9018@naver.com>
2021-04-24 16:57:10 +0900

    import 안쓰는 것 정리

--- Hoon/AndroidStudy_Hoon/app/src/main/java/com/study/androidstudy_hoon/MainActivity.kt
+++ Hoon/AndroidStudy_Hoon/app/src/main/java/com/study/androidstudy_hoon/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.study.androidstudy_hoon
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
--- Hoon/AndroidStudy_Hoon/app/src/main/java/com/study/androidstudy_hoon/presenter/search/DetailFragment.kt
+++ Hoon/AndroidStudy_Hoon/app/src/main/java/com/study/androidstudy_hoon/presenter/search/DetailFragment.kt
@@ -1,7 +1,6 @@
 package com.study.androidstudy_hoon.presenter.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
